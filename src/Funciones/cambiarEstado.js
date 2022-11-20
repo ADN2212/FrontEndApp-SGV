097b0885052d46d8a96b2b664188db19c95983e2
Juan Adonis Núñez Collado <adn221294@gmail.com>
2022-11-20 19:42:12 -0400

    Delete src directory

--- src/Funciones/cambiarEstado.js
+++
@@ -1,9 +0,0 @@
-function cambiarEstado(value, setterValue) {
-	/*
-		Esta función cambia el estado de los hooks para que se desplieguen o retraigan las opcinoes de editar y borrar.
-	*/
-	setterValue(!value);
-	return;
-}
-
-export default cambiarEstado;
--- src/Funciones/isLogin.js
+++
@@ -1,4 +0,0 @@
-export default function isLogin(activeUser) {
-	//Detremina si el user está login o no
-	return !(Object.keys(activeUser).length === 0);
-}
